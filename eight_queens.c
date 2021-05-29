2453ba0019b275ce1fa5df2bcc5589a4a9a48e03
jyygxxf <jyygxxf@163.com>
2021-05-29 14:04:18 +0800

    ok

--- eight_queens.c
+++ eight_queens.c
@@ -1,10 +1,8 @@
 #include "eight_queens.h"
 
 int main(void) {
-	board_init(1,8);
-	for (size_t row = 0;row < N;row++) {
-		put(row);
-	}
+	board_init(0,7);
+	put(0);
 	printf("%d\n", num);
 	return 0;
 }
@@ -12,33 +10,37 @@ int main(void) {
 void put(int row) {
 	if (row == N) {
 		num++;
-		board_init(8, 8);
+		result = (int***)realloc(result,num*sizeof(int));
+		result[num-1] = (int**)malloc(sizeof(int)*N);
+		for (size_t i = 0;i < N;i++) {
+			result[num - 1][i] = (int*)malloc(sizeof(int) * N);
+		}
+		copy(num);
 		return;
 	}
 	for (size_t column = 0;column < N;column++) {
-		if (!judge(row, column))
-			put(row++);
-		if (column == N - 1)return;
+		if (!judge(row, column)) {
+			put(row+1);
+			board_init(row, row);
+		}
 	}
 }
 
 int judge(int row,int column) {
 	board[row][column] = 1;
-	if (judge_column() + judge_bias(row, column)) {
+	if (judge_column(column) + judge_bias(row, column)) {
 		board[row][column] = 0;
 		return 1;
 	}
 	return 0;
 }
 
-int judge_column(void) {
+int judge_column(int column) {
+	int sum = 0;
 	for (size_t i = 0;i < N;i++) {
-		int sum = 0;
-		for (size_t j = 0;j < N;j++) {
-			sum += board[i][j];
-		}
-		if (sum > 1)return 1;
+			sum += board[i][column];
 	}
+	if (sum > 1)return 1;
 	return 0;
 }
 
@@ -62,4 +64,22 @@ int judge_bias(int row,int column) {
 	} while (row < 8 && column < 8);
 	if (sum > 2)return 1;
 	return 0;
+}
+
+void copy(int num) {
+	for (size_t i = 0;i < N;i++) {
+		for (size_t j = 0;j < N;j++) {
+			result[num - 1][i][j] = board[i][j];
+		}
+	}
+}
+
+void print(int num) {
+	for (size_t i = 0;i < N;i++) {
+		for (size_t j = 0;j < N;j++) {
+			printf("%d ", result[num - 1][i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
--- eight_queens.h
+++ eight_queens.h
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 #define N 8
 
@@ -6,10 +9,13 @@ int board[N][N];
 int num=0;
 void put(int);
 int judge(int,int);
-int judge_column(void);
+int judge_column(int);
 int judge_bias(int,int);
+int*** result = NULL;
+void copy(int);
+void print(int);
 
-#define board_init(x,y) for (size_t i = x-1;i < y ;i++) {\
+#define board_init(x,y) for (size_t i = x;i <= y ;i++) {\
 for (size_t j = 0;j < N;j++) {\
 	board[i][j] = 0;\
 }\
